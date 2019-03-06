12390ca5b82fa42329cee812d24ec6ce8aee5dd6
Alex Kogan <alx.kogan@gmail.com>
2019-03-06 13:01:43 +0100

    adding postgres database

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
+const knex = require('knex');
 
 const server = express();
+
+const PGserver = '127.0.0.1';
+const PGuser = 'postgres';
+const PGpassword = 'Alex1885';
+const PGdatabase = 'smart-brain';
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : PGserver,
+    user : PGuser,
+    password : PGpassword,
+    database : PGdatabase
+  }
+});
 
 server.use(bodyParser.json())
 server.use(cors());
@@ -37,54 +53,75 @@ server.get('/', (req, res) => {
 
 server.post('/signin', (req, res) => {
 	const {email, password} = req.body;
-	if (email === database.users[0].email && 
-		 password === database.users[0].password ){
-			res.json(database.users[0])
-		}
-		else {
-			res.status(400).json('error signing in')
-		}
+	db.select('email', 'hash').from('login')
+		.where('email','=', email)
+		.then(data => {
+			bcrypt.compare(password, data[0].hash, (error, passwordValid) => {
+				if (passwordValid) {
+					db.select('*').from('users')
+						.where('email','=', email)
+						.then(data => {
+							res.json(data[0])
+						})
+						.catch(error => res.status(400).json('could not get user'))
+				}
+				else
+					res.status(400).json("wrong password")
+			});
+		})
+		.catch(error => res.status(400).json("email does not exsit in system"))
 })
 
 server.post('/register', (req, res) => {
 	const {email, name, password} = req.body; 
-	database.users.push({
-		id: '125',
-		name: name,
-		email: email,
-		//password: password, not how we store passwords
-		entries: 0,
-		joined: new Date()
+	const hash = bcrypt.hash(password, null, null, (error, hash) => {
+		db.transaction(trx => {
+			trx.insert({
+				hash: hash,
+				email: email
+			})
+			.into('login')
+			.returning('email')
+			.then(loginEmail => {
+				return trx.insert({ // trx is avialble in this scope but needs to be returned to the parent scope so it can be commited
+						email: loginEmail[0], // since it's returning an array
+						name: name,
+						joined: new Date()
+					})
+					.into('users')
+					.returning('*')
+					.then(user => {
+						res.json(user[0])
+					})
+			})
+			.then(trx.commit)
+			.catch(trx.rollback)
+			})
+		.catch(error => res.status(400).json(error))
 	})
-	res.json(database.users[database.users.length-1])
 })
 
 server.get('/profile/:id', (req, res) => {
 	const {id} = req.params;
-	let found = false;
-	database.users.forEach( (user) => {
-		if (user.id===id) {
-			found = true;
-			return res.json(user);
-		}
-	})
-	if (!found) {
-		res.status(400).json('user not found')
-	}
+	db.select('*').from('users').where({id: id})
+		.then(user => {
+			if (user.length!==0)
+				res.json(user[0])
+			else
+				res.status(400).json('no such user')
+		})
+		.catch(error => res.status(400).json('error getting user'))
 });
 
 server.put('/image', (req, res) => {
 	const {id} = req.body;
-	let found = false;
-	database.users.forEach( (user) => {
-		if (user.id===id) {
-			found = true;
-			return res.json(++user.entries);
-		}
+	db('users').where('id', '=', id)
+	.increment('entries',1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0])
 	})
-	if (!found) {
-		res.status(400).json('user not found')
-	}
+	.catch(error => res.status(400).json('unable to get entries'))
 })
 
 server.listen(3000, () => {
